e34ce57950c50bc306b88bc77bf250650ff8d216
waqasbcs <waqasaliy2945@gmail.com>
2024-01-02 15:02:00 +0500

    completed

--- Resume/settings.py
+++ Resume/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'django_unused_media',
+    
 ]
 
 MIDDLEWARE = [
--- app/views.py
+++ app/views.py
@@ -4,21 +4,19 @@ from .models import Resume
 from django.views import View
 
 class HomeView(View):
- def get(self, request):
-  form = ResumeForm()
-  candidates = Resume.objects.all()
-  return render(request, 'home.html', { 'candidates':candidates, 'form':form})
+    def get(self, request):
+        form = ResumeForm()
+        candidates = Resume.objects.all()
+        return render(request, 'home.html', {'candidates': candidates, 'form': form})
 
-
-def post(self, request):
+    def post(self, request):
         form = ResumeForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')  
-        else:
-            candidates = Resume.objects.all()
-            return render(request, 'home.html', {'form': form, 'candidates': candidates})
+            # Redirect to the home page after successfully saving the form
+            return redirect('home')  # Replace 'home' with your URL name for the home page
+
 class CandidateView(View):
- def get(self, request, pk):
-  candidate = Resume.objects.get(pk=pk)
-  return render(request, 'candidate.html', {'candidate':candidate})
+    def get(self, request, pk):
+        candidate = Resume.objects.get(pk=pk)
+        return render(request, 'candidate.html', {'candidate': candidate})
